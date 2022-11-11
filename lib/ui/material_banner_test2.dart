6252ca739833f8113236a81a2f9e8772f8acd957
Tomoki Hasegawa <t-hase@nttr-tech.co.jp>
2022-11-12 02:04:05 +0900

    MaterialBannerのカウントダウンバージョンの実装を追加

--- lib/ui/material_banner_test2.dart
+++ lib/ui/material_banner_test2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_message_test/bloc/count_down_bloc.dart';
 
 class MaterialBannerTest2 extends StatefulWidget {
   const MaterialBannerTest2({super.key});
@@ -8,9 +10,54 @@ class MaterialBannerTest2 extends StatefulWidget {
 }
 
 class _MaterialBannerTest2State extends State<MaterialBannerTest2> {
+  final _countDownBloc = CountDownBloc();
+
+  @override
+  void dispose() {
+    _countDownBloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _countDownBloc.input.add(null);
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text("MaterialBanner Test 2")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("メッセージ表示まであと"),
+            StreamBuilder(
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data! == 0) {
+                  SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+                    ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(
+                      content: const Text("0になりました！"),
+                      actions: [
+                        TextButton(
+                          child: const Text("再スタート"),
+                          onPressed: () {
+                            ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+                            _countDownBloc.input.add(null);
+                          },
+                        )
+                      ],
+                    ));
+                  });
+                }
+                return Text("${snapshot.data}", style: const TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold));
+              },
+              stream: _countDownBloc.output,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
